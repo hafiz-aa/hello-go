fc99ac52045ee0197982a27334c59e6f33bd5f04
hafiz-aa <mhafiz.aa@gmail.com>
2021-10-13 14:34:47 +0700

    First Commit

---
+++ projectA/main.go
@@ -0,0 +1,71 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var angkaku, angkamu int = 3, 5
+	// nilaiku := 100
+	// katahatiku := "Ada yang jomblo?"
+
+	// fmt.Println(angkamu % angkaku)
+	// fmt.Println("Nilai ku adalah: ")
+	// fmt.Println(nilaiku)
+	// fmt.Println(katahatiku)
+
+	// a := 10
+	// t := 15
+	// L := (a * t) / 2
+
+	// fmt.Println(L)
+
+	// helloworld := "hello" + " " + "world"
+	// fmt.Println(helloworld)
+
+	// myAge := 17
+
+	// if dadAge := 40; dadAge < myAge {
+	// 	fmt.Println(dadAge)
+	// } else {
+	// 	fmt.Println("saya tua")
+	// }
+
+	// angka := 10
+
+	// if angka%2 == 0 {
+	// 	fmt.Println("GENAP")
+	// } else {
+	// 	fmt.Println("GANJIL")
+	// }
+
+	// for i := 0; i < 5; i++ {
+	// 	fmt.Println(i)
+	// }
+	// sum := 1
+	// for sum < 10 {
+	// 	sum += sum
+	// }
+	// fmt.Println(sum)
+
+	// cekBilanganPrima := 10
+	// habisBagi := 0
+
+	// for i := 1; i <= cekBilanganPrima; i++ {
+	// 	if cekBilanganPrima%i == 0 {
+	// 		habisBagi++
+	// 	}
+	// }
+
+	// if habisBagi == 2 {
+	// 	fmt.Println("PRIMA")
+	// } else {
+	// 	fmt.Println("BUKAN PRIMA")
+	// }
+
+	input := 6
+
+	if input%2 != 1 && input > 0 {
+		fmt.Println("banana")
+	} else {
+		fmt.Println("apple")
+	}
+}
